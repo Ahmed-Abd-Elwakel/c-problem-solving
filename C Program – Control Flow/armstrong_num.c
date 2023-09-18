28b224eba9e6a74396406f35ab6d66492ee34e68
kilo <ahmedabdelwakel4@gmail.com>
2023-09-19 00:12:04 +0300

    Finish this code
    so hard , and i add feature on it  , it check armstrong number for any number of digit

--- C Program – Control Flow/armstrong_num.c
+++ C Program – Control Flow/armstrong_num.c
@@ -2,21 +2,33 @@
 #include <stdio.h>
 #include <math.h>
 
-unsigned int Number,count ;
+unsigned int Number,count = 0 ;
+unsigned int P = 0;
+unsigned int Temp  ;
 
 int main()
 {
     printf("Enter Number :");
-    scanf("%d",Number);
+    scanf("%d",&Number);
 
+    Temp = Number ;
 
 //to get number of digit in variable = count;
     while (Number != 0) {
         Number = Number / 10; // Divide the number by 10
         count++; // Increment the count for each division
     }
-    //Working on  it 
-    if(pow())
+
+    Number = Temp ;
+
+    while (Number > 0) {
+    int rem = Number  % 10;
+    P = (P) + (pow(rem, count));
+    Number /=10 ;
+    }
+
+    if(Temp == P) printf("Yes , This Number %d is an Armstrong Number \n",Temp);
+    else printf("No , This Number %d NOT an Armstrong Number \n",Temp);
 
     return 0;
 }
